4c5e2dd4cb2e44e1c8be45c27cb082d14d81db6c
Roniel Valdez <roniel_valdez@outlook.com>
2026-01-08 16:20:07 +0100

    reset fork

--- feature/linkspeed/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Tailscale Inc & AUTHORS
-// SPDX-License-Identifier: BSD-3-Clause
-
-// Package linkspeed registers support for setting the TUN link speed on Linux,
-// to better integrate with system monitoring tools.
-package linkspeed
